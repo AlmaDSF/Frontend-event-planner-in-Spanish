6197eaaa4dd74fac2924d57747103c5cf3f1a524
Alma <almafernandez@yahoo.com>
2023-01-27 14:23:01 -0800

    Fixed connection with database and newsletter form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 
 import './App.css';
+import axios from "axios";
+import { useState } from "react";
 import SmartCalendar from './components/SmartCalendar';
 import FormContactInfo from './components/FormContactInfo';
 
 
 function App() {
 
+  const [contactList, setContactList] = useState([]);
+  const postman_url = "http://127.0.0.1:5000/contact_info";
+
+// form -> send new contact info
+
+  const addNewContactInfo = (newContactInfo) => {
+    axios
+      .post(`${postman_url}`, newContactInfo)
+      .then((response) => {
+        const newContactForm = [...contactList];
+        const newContactJson = {
+          ...newContactInfo,
+          id: response.data.id,
+        };
+        newContactForm.push(newContactJson);
+        setContactList(newContactForm);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+
+
   return (
     <div className="App">
       <h1 className="App-header"> "Calendario de eventos:" </h1>
         
-        <FormContactInfo />
+        <FormContactInfo addContactCallbackFunc={addNewContactInfo} />
 
         <SmartCalendar />
 
